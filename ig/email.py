c9ca055f206e56b403edea86e650b31c05859903
Dnmrk4 <danmark.chemuren@gmail.com>
2019-09-04 08:26:56 +0300

    send email

--- ig/email.py
+++ ig/email.py
@@ -1,14 +1,14 @@
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 
-def send_welcome_email():
+def send_welcome_email(name,receiver):
     # Creating message subject and sender
     subject = 'Welcome to Instagram'
-    sender = 'jonwanjema@gmail.com'
+    sender = 'danmark.mutai@gmail.com'
     print('Welcome to Instagram')
     #passing in the context vairables
-    text_content = render_to_string('email/email.txt',)
-    html_content = render_to_string('email/email.html',)
+    text_content = render_to_string('email/email.txt',{"name":name})
+    html_content = render_to_string('email/email.html',{"name":name})
 
     msg = EmailMultiAlternatives(subject,text_content,sender,[receiver])
     msg.attach_alternative(html_content,'text/html')
